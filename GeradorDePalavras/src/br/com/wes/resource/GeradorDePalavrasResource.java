3552d4b26eb69a652bd2401eba9182b695ae1a9c
WesRaiuga <weslley_wenny@hotmail.com>
2019-09-22 19:48:54 -0300

    Adição de mais algumas palavras.

--- GeradorDePalavras/src/br/com/wes/resource/GeradorDePalavrasResource.java
+++ GeradorDePalavras/src/br/com/wes/resource/GeradorDePalavrasResource.java
@@ -50,19 +50,18 @@ public class GeradorDePalavrasResource {
 				new String[] {"Amarelo","Anil","Azul","Bege","Bordô","Branco","Cáqui","Carmesim","Castanho","Ciano","Cinza","Dourado","Escarlate","Esmeralda","Fúcsia","Índigo","Laranja","Lilás","Magenta","Marrom","Prata","Preto","Púrpura","Rosa","Roxo","Rubro","Turquesa","Verde","Vermelho","Vinho","Violeta"}));
 		bancoDePalavras.add(new Theme("Cozinha",
 				new String[] {"Fogão","Geladeira","Microondas","Armário","Pratos","Talheres","Panelas"}));
-		bancoDePalavras.add(new Theme("Esportes",new String[] {"c","d"}));
-		bancoDePalavras.add(new Theme("Filmes",new String[] {"a","b"}));
+		bancoDePalavras.add(new Theme("Esportes",new String[] {"Arco e Flecha","Artes Marciais","Atletismo","Automobilismo","Badminton","Basquete","Beisebol","Bilhar","Bocha","Bodyboard","Boliche","Boxe","Bumerangue","Caça","Ciclismo","Corrida","Esgrima","Esqui Aquático","Frescobol","Frisbee","Futebol","Futebol Americano","Futevôlei","Ginástica","Golf","Handebol","Hóquei","Levantamento de Peso","Luta Livre","Mergulho","Montanhismo","Motociclismo","Natação","Navegação","Paintball","Patinação","Pesca","Pólo Aquático","Remo","Rugby","Skate","Squash","Surf","Tchoukball","Tênis","Tênis de Mesa","Tourada","Triathlon","Vôlei","Wakeboard"}));
+		bancoDePalavras.add(new Theme("Filmes",new String[] {"O Poderoso Chefão","O Mágico de Oz","Cidadão Kane","Pulp Fiction Tempo de Violência","Casablanca","2001 Uma Odisseia no Espaço","A Lista de Schindler","De Volta para o Futuro","Forrest Gump O Contador de Histórias","E o Vento Levou","O Silêncio dos Inocentes","Tubarão","A Noviça Rebelde","Cantando na Chuva","Clube dos Cinco","Blade Runner O Caçador de Andróides","Star Wars","Beleza Americana","Laranja Mecânica","Curtindo a Vida Adoidado","O Iluminado","Clube da Luta","Psicose","Alien o 8 Passageiro","Toy Story","Matrix","Titanic","O Resgate do Soldado Ryan","Janela Indiscreta","Jurassic Park","Brilho Eterno de uma Mente sem Lembranças","Batman O Cavaleiro das Trevas","O Fabuloso Destino de Amelie Poulain","O Rei Leão","Avatar","Monty Python e o Cálice Sagrado","Gladiador","Um Corpo que Cai","O Segredo de Brokeback Mountain","Os Caça Fantasmas","O Senhor dos Anéis","Duro de Matar","A Origem","Seven Os Sete Crimes Capitais","A Bela e a Fera","Quem Quer ser um Milionário","Amnésia","Up Altas Aventuras","Contatos Imediatos do Terceiro Grau","O Franco Atirador","O Labirinto do Fauno"}));
 		bancoDePalavras.add(new Theme("Frutas",new String[] {"Abacate","Abacaxi","Açaí","Acerola","Ameixa","Amêndoa","Amora","Ananás","Atemoia","Avelã","Banana","Bergamota","Cacau","Cajá","Caju","Carambola","Cereja","Ciriguela","Coco","Cranberry","Cupuaçu","Damasco","Figo","Framboesa","Fruta do conde","Fruta pão","Goiaba","Graviola","Groselha","Guaraná","Jabuticaba","Jaca","Jatobá","Jenipapo","Kiwi","Laranja","Limão","Lima","Lichia","Maçã","Macadâmia","Mamão","Manga","Maracujá","Marmelo","Melancia","Melão","Mexerica","Mirtilo","Morango","Nectarina","Pera","Pêssego","Pitanga","Pinha","Pitaia","Pitomba","Pistache","Romã","Tangerina","Tamarindo","Toranja","Tucumã","Uva"}));
-		bancoDePalavras.add(new Theme("Games",new String[] {"c","d"}));
-		bancoDePalavras.add(new Theme("Gêneros Musicais",new String[] {"c","d"}));
-		bancoDePalavras.add(new Theme("Hospital",new String[] {"c","d"}));
-		bancoDePalavras.add(new Theme("Instrumentos Musicais",new String[] {"c","d"}));
-		bancoDePalavras.add(new Theme("Países",new String[] {"c","d"}));
-		bancoDePalavras.add(new Theme("Praia",new String[] {"c","d"}));
-		bancoDePalavras.add(new Theme("Profissões",new String[] {"c","d"}));
-		bancoDePalavras.add(new Theme("Roupas",new String[] {"c","d"}));
-		bancoDePalavras.add(new Theme("Sala de Aula",new String[] {"c","d"}));
-		bancoDePalavras.add(new Theme("Séries de TV",new String[] {"c","d"}));
+//		bancoDePalavras.add(new Theme("Games",new String[] {"c","d"}));
+		bancoDePalavras.add(new Theme("Gêneros Musicais",new String[] {"Rock","Folk","Música Eletrônica","Jazz","Hip Hop","Rap","Reggae","Sertanejo","Samba","Pagode","Metal"}));
+		bancoDePalavras.add(new Theme("Instrumentos Musicais",new String[] {"agogô","atabaque","bateria","baixo","bongô","castanhola","chocalho","cuíca","flauta","gaita","guitarra","harpa","pandeiro","piano","reco reco","saxofone","surdo","tambor","tamborim","tantã","triângulo","viola","violão","violino","violoncelo"}));
+//		bancoDePalavras.add(new Theme("Países",new String[] {"c","d"}));
+//		bancoDePalavras.add(new Theme("Praia",new String[] {"c","d"}));
+//		bancoDePalavras.add(new Theme("Profissões",new String[] {"c","d"}));
+//		bancoDePalavras.add(new Theme("Roupas",new String[] {"c","d"}));
+//		bancoDePalavras.add(new Theme("Sala de Aula",new String[] {"c","d"}));
+//		bancoDePalavras.add(new Theme("Séries de TV",new String[] {"c","d"}));
 		
 		return bancoDePalavras;
 	}
